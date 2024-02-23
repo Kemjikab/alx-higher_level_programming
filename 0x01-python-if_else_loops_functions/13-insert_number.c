4c4f7163b8c456a3febeb299a1cd87fa54d98051
kemjikab <kemjikab26@gmail.com>
2024-02-23 14:56:01 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,32 +1,25 @@
 #include "lists.h"
 
 /**
- *  * insert_node - Inserts a number into a sorted singly-linked list.
- *   * @head: A pointer the head of the linked list.
- *    * @number: The number to insert.
- *     * Return: 0 If the function fails or pointer to the new node.
- *      */
+* insert_node - inserts a number into a sorted singly linked list
+* @head: pointer to head node
+* @number: value to be inserted
+*
+* Return: address of new node or NULL on failure
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
-		listint_t *node = *head, *new;
-
-			new = malloc(sizeof(listint_t));
-				if (new == NULL)
-							return (NULL);
-					new->n = number;
-
-						if (node == NULL || node->n >= number)
-								{
-											new->next = node;
-													*head = new;
-															return (new);
-																}
-
-							while (node && node->next && node->next->n < number)
-										node = node->next;
-
-								new->next = node->next;
-									node->next = new;
-
-										return (new);
+		listint_t *current = &head;
+		listint_t *next = current->next;
+		while (current->next != '\0')
+		{
+				if (next->n > number)
+				{
+						current->n = number;
+						current->next = current->next;
+						return (current);
+				}
+				current = current->next;
+		}
+		return (NULL);
 }
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-        if number < 0:
-                    last_digit = number % -(10)
-                            print(-(last_digit), end='')
-                                else:
-                                            last_digit = number % 10
-                                                    print(last_digit, end='')
-                                                        return abs(last_digit)
+    exe = 0
+    if number < 0:
+        number *= -1
+    exe = 1
+    last = number % 10
+    if exe == 1:
+        number *= -1
+    print("{:d}".format(last), end="")
+    return last
